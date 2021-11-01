796d60873d9ba7b8a190ae346d041bda44f0646e
devinafitri <devina.fitri@ui.ac.id>
2021-11-02 01:08:54 +0700

    new

--- tkpbp/urls.py
+++ tkpbp/urls.py
@@ -22,20 +22,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('konfirmasi/', include('konfirmasi.urls')),
+    path('konfirmasi/', include(('konfirmasi.urls', 'konfirmasi'), namespace='konfirmasi')),
     path('',include(accounts)),
     path('artikel/', include(('article.urls','article'), namespace='article')),
-<<<<<<< HEAD
-<<<<<<< HEAD
     re_path(r'^$', index_konfirmasi, name='index'),
-=======
-=======
->>>>>>> 7fb4f5bad6c4fc9ac7d5c8c2ea3d943bd53092f4
     path('consultation/', include(('consultation_form.urls', 'consultation_form'), namespace='consultation_form')),
     # re_path(r'^$', index_article, name='index'),
->>>>>>> 7fb4f5bad6c4fc9ac7d5c8c2ea3d943bd53092f4
-
-
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
